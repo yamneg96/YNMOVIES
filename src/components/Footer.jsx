8219523f35a428d10933141cdc2bd5dcf3f5e69b
Yamlak Negash <yamlaknegash96@gmail.com>
2025-07-03 22:19:24 +0300

    Added content for footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,28 @@ const Footer = () => {
   return (
     <div className='border-2 border-amber-50 bg-black flex-col'>
     <div className='text-white text-3xl text-left font-sans'>
-      <span className='text-cyan-600'>YNMOVIES</span> : Welcome, where you can find your favourite movies here 😊.
+      <span className='text-cyan-600'>YNMOVIES</span> 
+      : Welcome, where you can find your favourite movies here 😊.
       </div>
-      <div>
-
+      <div className='flex '>
+      <span className='text-cyan-600'>Countries </span>
+        <ul className='text-white'>
+          <li>USA</li>
+          <li>UK</li>
+          <li>ASIA</li>
+        </ul>
+      <span className='text-cyan-600'>Genres </span>
+      <ul className='text-white'>
+          <li>USA</li>
+          <li>UK</li>
+          <li>ASIA</li>
+        </ul>
+      <span className='text-cyan-600'>Contact </span>
+      <ul className='text-white'>
+          <li>USA</li>
+          <li>UK</li>
+          <li>ASIA</li>
+        </ul>
       </div>
     </div>
   )
