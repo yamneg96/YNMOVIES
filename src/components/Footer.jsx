b70a7f9259c578073c6e91014a1022736c60c9ab
Yamlak Negash <yamlaknegash96@gmail.com>
2025-07-03 22:10:40 +0300

    Added content for footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='border-2 border-amber-50 bg-black'>
-    <div className='text-white text-3xl text-center'>Footer</div>
+    <div className='border-2 border-amber-50 bg-black flex-col'>
+    <div className='text-white text-3xl text-left font-sans'>
+      YNMOVIES: Welcome, where you can find your favourite movies here 😊.
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
