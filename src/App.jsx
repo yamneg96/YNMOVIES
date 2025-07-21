de3888e106cb9ad0b1f254d5606066f976c7d333
Yamlak Negash <yamlaknegash96@gmail.com>
2025-07-21 15:37:28 +0300

    Fixed some deploy errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import Search from './components/Search.jsx'
+import Search from './components/search.jsx'
 import Spinner from './components/Spinner.jsx'
 import MovieCard from './components/MovieCard.jsx'
 import { useDebounce } from 'react-use'
